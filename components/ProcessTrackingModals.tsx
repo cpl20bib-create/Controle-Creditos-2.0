39fae90c9ae45000154b885f1711ea4ec714a939
cpl20bib-create <cpl20bib@gmail.com>
2026-07-28 16:25:08 -0300

    Update ProcessTrackingModals.tsx

--- components/ProcessTrackingModals.tsx
+++ components/ProcessTrackingModals.tsx
@@ -76,37 +76,29 @@ export const ProcessTrackingModals: React.FC<ProcessTrackingModalsProps> = ({ co
         }
       };
 
+      const typeLabel = isGlobal ? 'Parcial' : 'Ordinário';
+
       if (modalType === 'ConfDoc') {
         // Needs to have arrived but not sent to ConfDoc
-        if (isGlobal) {
-          (com.materialArrivals || []).forEach((arr: any) => {
+        if (com.materialArrivals && com.materialArrivals.length > 0) {
+          com.materialArrivals.forEach((arr: any) => {
             if (!arr.sentToConfDocDate) {
-              addGroupItem('Parcial', arr.invoice || 'S/N', arr.value, arr.id, arr.date);
+              addGroupItem(typeLabel, arr.invoice || 'S/N', arr.value, arr.id, arr.date);
             }
           });
-        } else {
-          if (com.materialArrivedDate && !com.sentToConfDocDate) {
-            const arr = com.materialArrivals?.[0];
-            const invoice = arr?.invoice || com.invoice || 'S/N';
-            const value = arr?.value || com.activeValue || com.value;
-            addGroupItem('Ordinário', invoice, value, arr?.id, com.materialArrivedDate);
-          }
+        } else if (com.materialArrivedDate && !com.sentToConfDocDate) {
+          addGroupItem(typeLabel, com.invoice || 'S/N', com.activeValue || com.value, undefined, com.materialArrivedDate);
         }
       } else {
         // Finance: Needs to be in ConfDoc but not sent to Finance
-        if (isGlobal) {
-          (com.materialArrivals || []).forEach((arr: any) => {
+        if (com.materialArrivals && com.materialArrivals.length > 0) {
+          com.materialArrivals.forEach((arr: any) => {
             if (arr.sentToConfDocDate && !arr.sentToFinanceDate) {
-              addGroupItem('Parcial', arr.invoice || 'S/N', arr.value, arr.id, arr.sentToConfDocDate);
+              addGroupItem(typeLabel, arr.invoice || 'S/N', arr.value, arr.id, arr.sentToConfDocDate);
             }
           });
-        } else {
-          if (com.sentToConfDocDate && !com.sentToFinanceDate) {
-            const arr = com.materialArrivals?.[0];
-            const invoice = arr?.invoice || com.invoice || 'S/N';
-            const value = arr?.value || com.activeValue || com.value;
-            addGroupItem('Ordinário', invoice, value, arr?.id, com.sentToConfDocDate);
-          }
+        } else if (com.sentToConfDocDate && !com.sentToFinanceDate) {
+          addGroupItem(typeLabel, com.invoice || 'S/N', com.activeValue || com.value, undefined, com.sentToConfDocDate);
         }
       }
     });
